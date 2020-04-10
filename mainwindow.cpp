4324fd548b2991a3387e71c0d7a11be38e5687e3
gforce0617 <artemmangutv@mail.ru>
2020-04-10 19:49:39 +0400

    finished, error

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -149,6 +149,7 @@ void MainWindow::cpy_lcdSlider_value()
     {
         slyder_value[i]=0;
     }
+    on_textBrowser_textChanged();
 }
 
 
@@ -172,12 +173,12 @@ void MainWindow::on_setchange_clicked()
 
 void MainWindow::on_textBrowser_textChanged()
 {
-    int fun;
+    int fun=0;
     QString f;
     for(int i=0;i<=spin_value;i++)
         fun+=pow(slyder_value[i],i);
     f=QString("%1^0+%2^1+%3^2+%4^3+%5^4+%6^5+%7^6+%8^7=%9").arg(slyder_value[0]).arg(slyder_value[1]).arg(slyder_value[2]).
             arg(slyder_value[3]).arg(slyder_value[4]).arg(slyder_value[5]).arg(slyder_value[6]).arg(slyder_value[7]).arg(fun);
     std::cout<<f.toStdString()<<std::endl;
-    //ui->textBrowser->setText();
+    ui->textBrowser->setText(f);
 }
